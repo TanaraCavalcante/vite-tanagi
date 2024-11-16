84af14a8c99385b8c561a7ecbe28e28337979435
TanaraCavalcante <tanaracavalcante@icloud.com>
2024-11-16 16:46:59 +0100

    loren in home

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -14,7 +14,16 @@ export default {
 
 <template>
   <section class="container">
-    <h2>Home</h2>
+    <div class="row">
+            <div class="col-12 my-3">
+                <h2>Home</h2>
+            </div>
+            <div class="row justify-content-center gap-2">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolor cumque libero tempora minima, fuga ex tenetur odio facilis velit exercitationem ipsa molestiae qui voluptatum voluptate voluptas deserunt, dolorem autem quis?Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum magni corrupti iusto officia aspernatur, optio animi, explicabo, eius aliquid esse ea voluptate perferendis nobis vero quam praesentium quod in debitis?
+                Dicta odio nam facilis minima dolorem soluta fuga quae maxime illo error, alias explicabo dolorum temporibus optio laborum corrupti natus? Deserunt excepturi provident accusantium possimus repellat dicta ut eaque distinctio!
+                Vero sunt voluptas exercitationem repellat expedita soluta at explicabo fuga, blanditiis molestiae, ducimus dolorum incidunt. Ex, quod culpa eligendi blanditiis sint corrupti asperiores, minus facilis possimus quo vero sit non.</p>
+            </div>
+        </div>
   </section>
 </template>
 
